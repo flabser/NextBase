306a6e4cdae3f8ec55adef73987d443cce5f9fa3
medin84 <kalihanovm@gmail.com>
2015-04-09 11:54:41 +0600

    убрал implements Const. изменил редирект, теперь зависит от настроек /nextbase/applications/@default, если не указан Workspace по умолчанию как и было

--- src/kz/flabs/servlets/Redirector.java
+++ src/kz/flabs/servlets/Redirector.java
@@ -1,31 +1,28 @@
 package kz.flabs.servlets;
 
-import kz.flabs.dataengine.Const;
-import kz.pchelka.env.Environment;
+import java.io.IOException;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import kz.pchelka.env.Environment;
 
 
-public class Redirector extends HttpServlet implements Const {
+public class Redirector extends HttpServlet {
+
 	private static final long serialVersionUID = 2107838212730208929L;
 
-	protected void  doGet(HttpServletRequest request, HttpServletResponse response){
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
 		doPost(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response){
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) {
 
 		try {
-			response.sendRedirect(Environment.getWorkspaceURL());
-		} catch (IOException e) {		
+			response.sendRedirect(Environment.getDefaultRedirectURL());
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-	}	
-
-
-
+	}
 }
