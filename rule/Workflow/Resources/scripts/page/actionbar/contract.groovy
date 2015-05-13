8789c5f8962f3ad3ac2354d9a58438a471f7896a
JohnFlabs <john_198708@mail.ru>
2015-05-13 18:02:13 +0600

    .

--- rule/Workflow/Resources/scripts/page/actionbar/contract.groovy
+++ rule/Workflow/Resources/scripts/page/actionbar/contract.groovy
@@ -13,7 +13,7 @@ class contract extends _DoScript {
 	public void doProcess(_Session session, _WebFormData formData, String lang) {
 		def actionBar = new _ActionBar(session);
 		def user = session.getCurrentAppUser();
-		if (user.hasRole(["registrator_projects", "administrator"])){
+		if (user.hasRole(["registrator_contract", "administrator"])){
 			def newDocAction = new _Action(getLocalizedWord("Новый договор", lang),getLocalizedWord("Новый договор", lang),"new_document")
 			newDocAction.setURL("Provider?type=edit&element=document&id=contract&docid=")
 			actionBar.addAction(newDocAction);
