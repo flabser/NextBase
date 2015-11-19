cbeaa2c737974bed933d35b5203908c2efc111ab
flabser <info@flabser.com>
2015-11-19 15:35:46 +0200

    console help was added

--- src/kz/pchelka/server/Console.java
+++ src/kz/pchelka/server/Console.java
@@ -1,8 +1,10 @@
 package kz.pchelka.server;
 
+import java.io.File;
 import java.util.Scanner;
 
 import kz.flabs.appenv.AppEnv;
+import kz.flabs.util.Util;
 import kz.pchelka.env.Environment;
 
 public class Console implements Runnable {
@@ -14,13 +16,15 @@ public class Console implements Runnable {
 		while (in.hasNext()) {
 			final String command = in.nextLine();
 			System.out.println("> " + command);
-			if (command.equalsIgnoreCase("quit") || command.equalsIgnoreCase("q")) {
+			if (command.equalsIgnoreCase("quit") || command.equalsIgnoreCase("exit") || command.equalsIgnoreCase("q")) {
 				Server.shutdown();
 				in.close();
 			} else if (command.equalsIgnoreCase("reset rules") || command.equalsIgnoreCase("rr")) {
 				for (AppEnv env : Environment.getApplications()) {
 					env.ruleProvider.resetRules();
 				}
+			} else if (command.equals("help") || command.equalsIgnoreCase("h")) {
+				System.out.println(Util.readFile("resources" + File.separator + "console_commands.txt"));
 			} else {
 				if (!command.trim().equalsIgnoreCase("")) {
 					System.err.println("command \"" + command + "\" is not recognized");
