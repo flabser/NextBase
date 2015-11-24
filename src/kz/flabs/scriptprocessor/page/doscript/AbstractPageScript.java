5a8f029c76dcbb9a7f78f41d2831f8c04b353002
flabser <info@flabser.com>
2015-11-24 17:43:22 +0200

    f

--- src/kz/flabs/scriptprocessor/page/doscript/AbstractPageScript.java
+++ src/kz/flabs/scriptprocessor/page/doscript/AbstractPageScript.java
@@ -1,5 +1,8 @@
 package kz.flabs.scriptprocessor.page.doscript;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import kz.flabs.localization.Vocabulary;
 import kz.flabs.scriptprocessor.Msg;
 import kz.flabs.scriptprocessor.ScriptEvent;
@@ -8,34 +11,38 @@ import kz.flabs.servlets.SignalType;
 import kz.flabs.util.ResponseType;
 import kz.flabs.util.Util;
 import kz.flabs.util.XMLResponse;
-import kz.nextbase.script.*;
+import kz.nextbase.script._IXMLContent;
+import kz.nextbase.script._Session;
+import kz.nextbase.script._Tag;
+import kz.nextbase.script._WebFormData;
+import kz.nextbase.script._XMLDocument;
 import kz.nextbase.script.reports._ExportManager;
+import kz.pchelka.server.Server;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
-public abstract class AbstractPageScript extends ScriptEvent implements IPageScript {	
+public abstract class AbstractPageScript extends ScriptEvent implements IPageScript {
 	public ArrayList<Msg> messages = new ArrayList<Msg>();
-	
 
 	private String lang;
 	private _WebFormData formData;
 	private XMLResponse xmlResp = new XMLResponse(ResponseType.RESULT_OF_PAGE_SCRIPT);
 	private SignalType signal;
-	private ArrayList<_IXMLContent> xml = new ArrayList<_IXMLContent>(); 
+	private ArrayList<_IXMLContent> xml = new ArrayList<_IXMLContent>();
 
-	public void setSession(_Session ses){			
+	@Override
+	public void setSession(_Session ses) {
 		this.ses = ses;
 	}
 
-	public void setFormData(_WebFormData formData){
+	@Override
+	public void setFormData(_WebFormData formData) {
 		this.formData = formData;
 	}
 
-	public void setCurrentLang(Vocabulary vocabulary, String lang){
+	@Override
+	public void setCurrentLang(Vocabulary vocabulary, String lang) {
 		this.lang = lang;
 		this.vocabulary = vocabulary;
-	}	
+	}
 
 	public void setContent(_IXMLContent document) {
 		xml.add(document);
@@ -45,46 +52,55 @@ public abstract class AbstractPageScript extends ScriptEvent implements IPageScr
 		xml.addAll(documents);
 	}
 
-	public void println(Exception e){
+	public void println(Exception e) {
 		String errText = e.toString();
 		System.out.println(errText);
-		_Tag tag = new _Tag("error",errText);
+		_Tag tag = new _Tag("error", errText);
 		tag.addTag(ScriptProcessorUtil.getScriptError(e.getStackTrace()));
 		_XMLDocument xml = new _XMLDocument(tag);
 		setContent(xml);
 	}
 
-	public void println(String text){		
+	public void println(Object text) {
 		System.out.println(text);
 	}
 
-    public void showFile(String filePath, String fileName) {
-        xmlResp.type = ResponseType.SHOW_FILE_AFTER_HANDLER_FINISHED;
-        xmlResp.addMessage(filePath, "filepath");
-        xmlResp.addMessage(fileName, "originalname");
-    }
-
-	public void showFile(_ExportManager attachment){
-		xmlResp.type = ResponseType.SHOW_FILE_AFTER_HANDLER_FINISHED;
-		xmlResp.addMessage(attachment.getFilePath(), "filepath");	
-		xmlResp.addMessage(attachment.getOriginalFileName(), "originalname");	
+	public void log(String text) {
+		Server.logger.normalLogEntry(text);
 	}
 
-	public XMLResponse process(){
+	public void showFile(String filePath, String fileName) {
+		xmlResp.type = ResponseType.SHOW_FILE_AFTER_HANDLER_FINISHED;
+		xmlResp.addMessage(filePath, "filepath");
+		xmlResp.addMessage(fileName, "originalname");
+	}
+
+	public void showFile(_ExportManager attachment) {
+		xmlResp.type = ResponseType.SHOW_FILE_AFTER_HANDLER_FINISHED;
+		xmlResp.addMessage(attachment.getFilePath(), "filepath");
+		xmlResp.addMessage(attachment.getOriginalFileName(), "originalname");
+	}
+
+	@Override
+	public XMLResponse process() {
 
 		long start_time = System.currentTimeMillis();
-		try{					
+		try {
 			doProcess(ses, formData, lang);
 			xmlResp.setPublishResult(toPublishElement);
 			xmlResp.setResponseStatus(true);
-			for(Msg message:messages){
-				xmlResp.addMessage(message.text, message.id);	
+			for (Msg message : messages) {
+				xmlResp.addMessage(message.text, message.id);
 			}
-			if (signal != null)	xmlResp.addSignal(signal);
-			if (xml != null)xmlResp.addXMLDocumentElements(xml);
+			if (signal != null) {
+				xmlResp.addSignal(signal);
+			}
+			if (xml != null) {
+				xmlResp.addXMLDocumentElements(xml);
+			}
 			xmlResp.setRedirect(redirectURL);
 
-		}catch(Exception e){
+		} catch (Exception e) {
 			xmlResp.setResponseStatus(false);
 			xmlResp.addMessage(e.getMessage());
 			println(e);
